ea0c749c9fa5411dca557589bdec619fa075412f
James <jamesrynn23@gmail.com>
2023-09-29 11:17:50 +1000

    Add Random Numbers program.

---
+++ prac_03/randoms.py
@@ -0,0 +1,13 @@
+# a.
+# 20, Smallest = 5, Largest = 20
+
+# b.
+# 3, Smallest = 3, Largest = 9, No
+
+# c.
+# 4.8466367161707105, Smallest = 2.5, Largest = 5.5
+
+# d.
+import random
+print(random.randint(1, 100))
+
