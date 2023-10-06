fb6f0fce155dccb50b64b6e72a927784885aefbb
James <jamesrynn23@gmail.com>
2023-10-06 16:13:12 +1000

    Add part 2 warm up answers

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -1,12 +1,22 @@
-# a.
-# 3
-# 2
-# 1
-# 3, 1, 4, 1, 5, 9
-# 4, 1
-# True
-# False
-# False
-# 3, 1, 4, 1, 5, 9, 2, 6, 5, 3
+numbers = [3, 1, 4, 1, 5, 9, 2]
 
+# Part 1
+# a. 3
+# b. 2
+# c. 1
+# d. 3, 1, 4, 1, 5, 9
+# e. 4, 1
+# f. True
+# g. False
+# h. False
+# i. 3, 1, 4, 1, 5, 9, 2, 6, 5, 3
+
+# Part 2
+# a.
+numbers[0] = "10"
 # b.
+numbers[-1] = 1
+# c.
+print(numbers[2:])
+# d.
+print(9 in numbers)
