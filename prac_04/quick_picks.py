49db843167b36a601d6828e48b191dc4955f77aa
James <jamesrynn23@gmail.com>
2023-10-06 16:00:07 +1000

    Add CONSTANTS to Quick Pick program

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,12 +1,15 @@
 import random
+AMOUNT_OF_NUMBERS = 6
+RANDOM_NUMBER_MINIMUM = 1
+RANDOM_NUMBER_MAXIMUM = 45
 
 number_of_picks = int(input("How many picks? "))
 for i in range(number_of_picks):
     number_list = []
-    for j in range(6):
-        random_number = random.randint(1, 100)
+    for j in range(AMOUNT_OF_NUMBERS):
+        random_number = random.randint(RANDOM_NUMBER_MINIMUM, RANDOM_NUMBER_MAXIMUM)
         while random_number in number_list:
-            random_number = random.randint(1, 100)
+            random_number = random.randint(RANDOM_NUMBER_MINIMUM, RANDOM_NUMBER_MAXIMUM)
         number_list.append(random_number)
     number_list.sort()
     print(" ".join(f"{random_number:2}" for random_number in number_list))
