a0cd2ee075219be02bab1c1017f2294a8a00aa0e
James <jamesrynn23@gmail.com>
2023-10-15 19:53:26 +1000

    Add South Australia to dictionary

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -5,7 +5,7 @@ File needs reformatting
 """
 
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
-                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
 print(CODE_TO_NAME)
 # a.
 state_code = input("Enter short state: ").upper()
