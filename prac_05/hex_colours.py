3d739a3f30bc7d30dca7c2111b161e634ba96cb2
James <jamesrynn23@gmail.com>
2023-10-15 20:09:26 +1000

    Updated to use exception handling

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -5,9 +5,9 @@ COLOUR_NAME_TO_CODE = {"aqua": "#00ffff", "black": "#000000", "blue": "#0018a8",
 print(COLOUR_NAME_TO_CODE)
 colour_name = input("Enter colour: ").lower()
 while colour_name != "":
-    if colour_name in COLOUR_NAME_TO_CODE:
+    try:
         print(colour_name, "is", COLOUR_NAME_TO_CODE[colour_name])
-    else:
+    except KeyError:
         print("Invalid")
     colour_name = input("Enter colour: ").lower()
 print("Chow")
