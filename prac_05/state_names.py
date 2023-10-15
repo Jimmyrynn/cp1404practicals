7b0b1a07f7caa8eaf2be8deadea104e41c7f0b86
James <jamesrynn23@gmail.com>
2023-10-15 19:56:03 +1000

    Remove magic number for CONSTANT

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -3,7 +3,7 @@ CP1404/CP5632 Practical
 State names in a dictionary
 File needs reformatting
 """
-
+WORD_GAP = 3
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
 print(CODE_TO_NAME)
@@ -18,7 +18,7 @@ while state_code != "":
 
 # b.
 for state_code in CODE_TO_NAME:
-    print(f"{state_code:3} is {CODE_TO_NAME[state_code]}")
+    print(f"{state_code:{WORD_GAP}} is {CODE_TO_NAME[state_code]}")
 
 # c.
 state_code = input("Enter short state: ").upper()
