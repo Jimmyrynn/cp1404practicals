b400aebebcef733a0868cef9408914dd5eac9ff5
James <jamesrynn23@gmail.com>
2023-10-15 20:17:45 +1000

    Simplified by combining code to avoid DRY

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -6,10 +6,8 @@ Actual: 22 minutes
 word_to_frequency = {}
 # text = input("Enter Text: ")
 text = "this is a collection of words of nice words this is a fun thing it is"
-words = text.split()
-for word in words:
-    frequency = word_to_frequency.get(word, 0)
-    word_to_frequency[word] = frequency + 1
+for word in text.split():
+    word_to_frequency[word] = word_to_frequency.get(word, 0) + 1
 words = list(word_to_frequency.keys())
 words.sort()
 longest_word = max((len(word) for word in words))
