3873df7265ecfc1683405ca132edd9328231951e
James <jamesrynn23@gmail.com>
2023-11-05 23:52:32 +1000

    Complete Week 7 project and project_management programs

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,5 +1,8 @@
 class Project:
+    """Represent a Project object."""
+
     def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
+        """Initialise a Project instance."""
         self.name = name
         self.start_date = start_date
         self.priority = priority
@@ -7,9 +10,16 @@ class Project:
         self.completion_percentage = completion_percentage
 
     def __repr__(self):
+        """Return a string containing project information."""
         return f"{self.name}, start: {self.start_date}, priority {self.priority}, estimate: ${self.cost_estimate}, " \
                f"completion: {self.completion_percentage}%"
 
+    def __gt__(self, other):
+        """Compare project years."""
+        return self.start_date > other.year
 
+    def update_percentage(self, new_percentage):
+        """Update percentage."""
+        self.completion_percentage = new_percentage
 
 
--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -7,31 +7,18 @@ import datetime
 
 
 def main():
-    filename = "projects.txt"
+    # filename = "projects.txt"
     projects = []
     print_menu()
     user_input = input(">>> ").lower()
     while user_input != 'q':
         if user_input == 'l':
-            # filename = input("Enter Filename: ")
-            filename = "projects.txt"
+            filename = input("Enter Filename: ")
             projects = []
-            in_file = open(filename, 'r')
-            in_file.readline()
-            for line in in_file:
-                parts = line.strip().split('\t')
-                project = Project(parts[0], parts[1], int(parts[2]), float(parts[3]), int(parts[4]))
-                projects.append(project)
-            in_file.close()
+            read_data(filename, projects)
 
         elif user_input == 's':
-            output_file = open("test.txt", 'w')
-            output_file.write(
-                f"Name\tStart Date\tPriority\tCost Estimate\tCompletion Percentage\n")
-            for project in projects:
-                output_file.write(
-                    f"{project.name}\t{project.start_date}\t{project.priority}\t{project.cost_estimate}\t{project.completion_percentage}\n")
-            output_file.close()
+            write_to_file(projects)
 
         elif user_input == 'd':
             completed_projects = []
@@ -41,43 +28,86 @@ def main():
                     incomplete_projects.append(project)
                 else:
                     completed_projects.append(project)
-            print("Uncompleted projects: ")
-            for project in incomplete_projects:
-                print(f" {project}")
-            print("Completed projects: ")
-            for project in completed_projects:
-                print(f" {project}")
+            print_projects(completed_projects, incomplete_projects)
 
-
-        elif user_input == 'f':
+        elif user_input == 'f':  # Incomplete
             date_string = input("Show projects that start after date (dd/mm/yy): ")
             date = datetime.datetime.strptime(date_string, "%d/%m/%Y").date()
-
+            filtered_projects = [project for project in projects if project.start_date > date]
+            for project in filtered_projects:
+                print(project)
 
         elif user_input == 'a':
             print("Let's add a new project")
-            name = input("Name: ")
-            date_string = input("Start date (dd/mm/yy): ")
-            start_date = datetime.datetime.strptime(date_string, "%d/%m/%Y").date()
-            priority = int(input("Priority: "))
-            cost_estimate = int(input("Cost estimate: $"))
-            completion_percentage = int(input("Percent complete: "))
-            projects.append(Project(name, start_date, priority, cost_estimate, completion_percentage))
-
-
+            add_project(projects)
 
         elif user_input == 'u':
-            print("u")
+            for i, project in enumerate(projects, 0):
+                print(f"{i} {project}")
+            update_project(projects)
         else:
             print("Error")
         print_menu()
         user_input = input(">>> ").lower()
 
 
+def update_project(projects):
+    """Update a project in a list."""
+    project_choice = int(input("Project Choice: "))
+    print(projects[project_choice])
+    new_percentage = int(input("New Percentage:"))
+    projects[project_choice].completion_percentage = new_percentage
+
+
+def add_project(projects):
+    """Add project to list."""
+    name = input("Name: ")
+    date_string = input("Start date (dd/mm/yy): ")
+    start_date = datetime.datetime.strptime(date_string, "%d/%m/%Y").date()
+    priority = int(input("Priority: "))
+    cost_estimate = int(input("Cost estimate: $"))
+    completion_percentage = int(input("Percent complete: "))
+    projects.append(Project(name, start_date, priority, cost_estimate, completion_percentage))
+
+
+def print_projects(completed_projects, incomplete_projects):
+    """Print complete & incomplete projects."""
+    print("Uncompleted projects: ")
+    for project in incomplete_projects:
+        print(f" {project}")
+    print("Completed projects: ")
+    for project in completed_projects:
+        print(f" {project}")
+
+
+def write_to_file(projects):
+    """Write to an output file."""
+    output_file = open("test.txt", 'w')
+    output_file.write(
+        f"Name\tStart Date\tPriority\tCost Estimate\tCompletion Percentage\n")
+    for project in projects:
+        output_file.write(
+            f"{project.name}\t{project.start_date}\t{project.priority}"
+            f"\t{project.cost_estimate}\t{project.completion_percentage}\n")
+    output_file.close()
+
+
+def read_data(filename, projects):
+    """Read data from file."""
+    in_file = open(filename, 'r')
+    in_file.readline()
+    for line in in_file:
+        parts = line.strip().split('\t')
+        project = Project(parts[0], parts[1], int(parts[2]), float(parts[3]), int(parts[4]))
+        projects.append(project)
+    in_file.close()
+
+
 def print_menu():
+    """Print menu."""
     print("(L)oad projects\n(S)ave projects\n(D)isplay projects\n"
           "(F)ilter projects by date\n(A)dd new project\n(U)pdate"
-          "project\n(Q)uit")
+          " project\n(Q)uit")
 
 
 main()
