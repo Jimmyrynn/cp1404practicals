f99afc84e95f175c1c891803c484d8774ec4a58f
James <jamesrynn23@gmail.com>
2023-11-05 14:51:34 +1000

    Add more tests

--- prac_07/programming_language.py
+++ prac_07/programming_language.py
@@ -26,9 +26,9 @@ class ProgrammingLanguage:
 
 def run_tests():
     """Run simple tests/demos on ProgrammingLanguage class."""
-    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
-    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
-    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995, True)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991, False)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991, True)
 
     languages = [ruby, python, visual_basic]
     print(python)
